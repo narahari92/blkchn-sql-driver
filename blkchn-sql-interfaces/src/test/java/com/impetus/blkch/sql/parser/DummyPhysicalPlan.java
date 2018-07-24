284d84446ee0ab4aa48f9d868218e5f140ce5302
ayush.vasu <ayush.vasu@impetus.co.in>
2018-07-24 15:06:08 +0530

    Spark Changes and cloumn type constant for List<String> and List<Int>

--- blkchn-sql-interfaces/src/test/java/com/impetus/blkch/sql/parser/DummyPhysicalPlan.java
+++ blkchn-sql-interfaces/src/test/java/com/impetus/blkch/sql/parser/DummyPhysicalPlan.java
@@ -15,6 +15,7 @@
 ******************************************************************************/
 package com.impetus.blkch.sql.parser;
 
+import java.sql.Types;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -67,8 +68,13 @@ public class DummyPhysicalPlan extends PhysicalPlan
     }
 
     @Override
-    public Map<String, Integer> columnTypeMap(String table) {
-        return new HashMap<>();
+    public Map<String, Integer> getColumnTypeMap(String table) {
+        Map<String, Integer> columnTypeMap = new HashMap<>();
+        columnTypeMap.put("column1", Types.BIGINT);
+        columnTypeMap.put("column2", Types.BIGINT);
+        columnTypeMap.put("qcol1", Types.VARCHAR);
+        columnTypeMap.put("qcol2", Types.VARCHAR);
+        return null;
     }
     
 
---
+++ spark-blkchn-connector/src/main/scala/com/impetus/blkch/spark/connector/rdd/BlkchnRDD.scala
@@ -0,0 +1,60 @@
+package com.impetus.blkch.spark.connector.rdd
+
+import java.sql.{ResultSetMetaData, Types}
+
+import com.impetus.blkch.spark.connector.rdd.partitioner.BlkchnPartition
+import com.impetus.blkch.spark.connector.BlkchnConnector
+import com.impetus.blkch.sql.BlkType
+import org.apache.spark.broadcast.Broadcast
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.types._
+import org.apache.spark.{Dependency, Partition, SparkContext, TaskContext}
+
+import scala.reflect.ClassTag
+
+class BlkchnRDD[R: ClassTag](@transient sc: SparkContext,
+                             private[connector] val connector: Broadcast[BlkchnConnector],
+                             private[connector] val readConf: ReadConf) extends RDD[R](sc, Nil) {
+
+  override protected def getPartitions: Array[Partition] = {
+    readConf.partitioner.getPartitions(connector.value, readConf).asInstanceOf[Array[Partition]]
+  }
+
+  override def compute(split: Partition, context: TaskContext): Iterator[R] = {
+    connector.value.withStatementDo {
+      stat =>
+        val partition = split.asInstanceOf[BlkchnPartition]
+        stat.setPageRange(partition.range)
+        val rs = stat.executeQuery(partition.readConf.query)
+        var buffer = scala.collection.mutable.ArrayBuffer[R]()
+        val metadata = rs.getMetaData
+        val columnCount = metadata.getColumnCount
+        while(rs.next()) {
+          val rowVals = (for(i <- 1 to columnCount) yield {
+            (rs.getObject(i).asInstanceOf[Any], getStructField(i, metadata))
+          }).toArray
+          buffer = buffer :+ new GenericRowWithSchema(rowVals.map(_._1), StructType(rowVals.map(_._2))).asInstanceOf[R]
+        }
+        buffer.toIterator
+    }
+  }
+
+  private def getStructField(index: Int, metadata: ResultSetMetaData): StructField = {
+    metadata.getColumnType(index) match {
+      case BlkType.JAVA_LIST_STRING => return StructField(metadata.getColumnLabel(index), ArrayType(StringType, true), true)
+      case BlkType.JAVA_LIST_INTEGER => return StructField(metadata.getColumnLabel(index), ArrayType(IntegerType, true), true)
+      case _=> val dataType = metadata.getColumnType(index) match {
+        case Types.INTEGER => IntegerType
+        case Types.BIGINT => DecimalType(38,0)
+        case Types.DOUBLE => DoubleType
+        case Types.FLOAT => FloatType
+        case Types.BOOLEAN => BooleanType
+        case _ => StringType
+      }
+        StructField(metadata.getColumnLabel(index), dataType, true)
+    }
+
+  }
+
+}
